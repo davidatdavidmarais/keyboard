92633f36ab546d1d6b3b7bb7e09928990b53857c
David Marais <david@davidmarais.co.za>
2024-04-05 17:35:23 +0200

    Update code.py

--- code.py
+++ code.py
@@ -29,21 +29,38 @@ keyboard.modules.append(oneshot)
 holdtap = HoldTap()
 holdtap.tap_time = 200
 
+_______ = KC.TRNS
+XXXXXXX = KC.NO
+
 keyboard.keymap = [
     # Base layer
     [
-     KC.TAB,                KC.Q,         KC.W,           KC.E,                  KC.R,                   KC.T,                                              KC.Y,           KC.U,          KC.I,           KC.O,          KC.P,           KC.LBRC,
-     KC.OS(KC.LALT),        KC.A,         KC.S,           KC.D,                  KC.F,                   KC.G,                                              KC.H,           KC.K,          KC.K,           KC.L,          KC.SCLN,        KC.QUOT,
-     KC.OS(KC.LSFT),        KC.Z,         KC.X,           KC.C,                  KC.V,                   KC.B,                                              KC.N,           KC.M,          KC.COMM,        KC.DOT,        KC.SLSH,        KC.RSFT,
-     KC.NO,                 KC.NO,        KC.NO,          KC.OS(KC.LCTL),        KC.LGUI(KC.ENT),        KC.HT(KC.TAB, KC.FD(1)),                                  KC.BSPC,        KC.ENT,        KC.MEH,         KC.NO,         KC.NO,          KC.NO
+     KC.TAB,                KC.Q,         KC.W,           KC.E,                  KC.R,                   KC.T,                                              KC.Y,                           KC.U,             KC.I,                   KC.O,          KC.P,           KC.DEL,
+     KC.OS(KC.LALT),        KC.A,         KC.S,           KC.D,                  KC.F,                   KC.G,                                              KC.H,                           KC.K,             KC.K,                   KC.L,          KC.QUOT,        KC.OS(KC.RALT),
+     KC.OS(KC.LSFT),        KC.Z,         KC.X,           KC.C,                  KC.V,                   KC.B,                                              KC.N,                           KC.M,             KC.COMM,                KC.DOT,        KC.SLSH,        KC.OS(KC.MO(3)),
+     XXXXXXX,               XXXXXXX,      XXXXXXX,        KC.OS(KC.LCTL),        KC.LCMD(KC.ENT),        KC.HT(KC.TAB, KC.FD(1)),                           KC.HT(KC.BSPC,KC.FD(2)),        KC.SPC,           KC.OS(KC.RSFT),         XXXXXXX,       XXXXXXX,        XXXXXXX
     ],
     # Lower layer
     [
-     KC.TRNS,      KC.TRNS,      KC.TRNS,      KC.TRNS,      KC.TRNS,      KC.TRNS,                                                                         KC.TRNS,      KC.TRNS,      KC.TRNS,      KC.TRNS,       KC.TRNS,      KC.TRNS,  
-     KC.NO,        KC.P1,        KC.P2,        KC.P3,        KC.P4,        KC.P5,                                                                           KC.P6,        KC.P7,        KC.P8,        KC.P9,         KC.P0,        KC.NO,  
-     KC.TRNS,      KC.TRNS,      KC.TRNS,      KC.TRNS,      KC.TRNS,      KC.TRNS,                                                                         KC.TRNS,      KC.TRNS,      KC.TRNS,      KC.TRNS,       KC.TRNS,      KC.TRNS,  
-     KC.NO,        KC.NO,        KC.NO,        KC.TRNS,      KC.TRNS,      KC.FD(0),                                                                        KC.TRNS,      KC.TRNS,      KC.TRNS,      KC.NO,         KC.NO,        KC.NO
+     _______,      KC.EXLM,      KC.AT,        KC.HASH,      KC.DLR,       KC.PERC,                                                                         KC.CIRC,      KC.AMPR,      KC.ASTR,      KC.LPRN,       KC.RPRN,      _______,
+     _______,      KC.P1,        KC.P2,        KC.P3,        KC.P4,        KC.P5,                                                                           KC.P6,        KC.P7,        KC.P8,        KC.P9,         KC.P0,        _______,
+     _______,      XXXXXXX,      KC.TILD,      KC.GRV,       KC.LBRC,      KC.LCBR,                                                                         KC.RCBR,      KC.RBRC,      KC.COMM,      KC.DOT,        KC.SLSH,      _______,
+     XXXXXXX,      XXXXXXX,      XXXXXXX,      _______,      _______,      KC.FD(0),                                                                        _______,      _______,      KC.COLN,      XXXXXXX,       XXXXXXX,      XXXXXXX
     ],
+    # Raise layer
+    [
+     _______,      KC.DEL,       XXXXXXX,      KC.UNDS,            KC.PLUS,            KC.PGUP,                                                             XXXXXXX,      XXXXXXX,     XXXXXXX,      KC.BSLS,       KC.PIPE,      _______,
+     _______,      KC.HOME,      KC.END,       KC.MINS,            KC.EQL,             KC.PGDN,                                                             KC.APP,       KC.LEFT,     KC.UP,        KC.RIGHT,      KC.DOWN,      _______,
+     _______,      KC.LABK,      KC.RABK,      KC.LCMD(KC.C),      KC.LCMD(KC.V),      KC.SCLN,                                                             KC.MPLY,      KC.MPRV,     KC.MNXT,      KC.VOLD,       KC.VOLU,      _______,
+     XXXXXXX,      XXXXXXX,      XXXXXXX,      KC.LCTL(KC.ESC),    _______,            XXXXXXX,                                                             KC.FD(0),     _______,     _______,      XXXXXXX,       XXXXXXX,      XXXXXXX
+    ],
+    # Func layer
+    [
+     _______,      KC.F1,         KC.F2,         KC.F3,        KC.F4,        KC.F5,                                                                          KC.F6,        KC.F7,         KC.F8,        KC.F9,         KC.F10,       _______,
+     _______,      KC.F11,        KC.F12,        XXXXXXX,      XXXXXXX,      XXXXXXX,                                                                        XXXXXXX,      XXXXXXX,       XXXXXXX,      XXXXXXX,       XXXXXXX,      _______,
+     _______,      XXXXXXX,       XXXXXXX,       XXXXXXX,      XXXXXXX,      XXXXXXX,                                                                        XXXXXXX,      XXXXXXX,       XXXXXXX,      XXXXXXX,       XXXXXXX,      XXXXXXX,
+     _______,      XXXXXXX,       XXXXXXX,       XXXXXXX,      XXXXXXX,      XXXXXXX,                                                                        XXXXXXX,      KC.FD(0),      XXXXXXX,      XXXXXXX,       XXXXXXX,      XXXXXXX
+    ]
 ]    
 
 if __name__ == '__main__':
