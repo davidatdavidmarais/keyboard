f91763d45751243f6c0e0faf4c712b1e543971f2
David Marais <david@davidmarais.co.za>
2024-04-11 11:02:47 +0200

    Update code.py

--- code.py
+++ code.py
@@ -36,10 +36,10 @@ XXXXXXX = KC.NO
 keyboard.keymap = [
     # Base layer
     [
-     KC.TAB,                KC.Q,         KC.W,           KC.E,                  KC.R,                          KC.T,                                       KC.Y,                           KC.U,             KC.I,                   KC.O,          KC.P,           KC.ESC,
-     KC.OS(KC.LALT),        KC.A,         KC.S,           KC.D,                  KC.F,                          KC.G,                                       KC.H,                           KC.J,             KC.K,                   KC.L,          KC.SCLN,        KC.QUOT,
-     KC.OS(KC.LSFT),        KC.Z,         KC.X,           KC.C,                  KC.V,                          KC.B,                                       KC.N,                           KC.M,             KC.COMM,                KC.DOT,        KC.SLSH,        KC.QUES,
-     XXXXXXX,               XXXXXXX,      XXXXXXX,        KC.OS(KC.MEH),         KC.HT(KC.ENT, KC.LGUI),        KC.FD(1),                                   KC.BSPC,                        KC.SPC,           KC.OS(KC.CTRL),         XXXXXXX,       XXXXXXX,        XXXXXXX
+     KC.HT(KC.TAB, KC.CTRL(KC.ALT)),                KC.Q,         KC.W,           KC.E,                  KC.R,                          KC.T,                                       KC.Y,                           KC.U,             KC.I,                   KC.O,          KC.P,           KC.ESC,
+     KC.OS(KC.LALT),                                KC.A,         KC.S,           KC.D,                  KC.F,                          KC.G,                                       KC.H,                           KC.J,             KC.K,                   KC.L,          KC.SCLN,        KC.QUOT,
+     KC.OS(KC.LSFT),                                KC.Z,         KC.X,           KC.C,                  KC.V,                          KC.B,                                       KC.N,                           KC.M,             KC.COMM,                KC.DOT,        KC.SLSH,        KC.QUES,
+     XXXXXXX,                                       XXXXXXX,      XXXXXXX,        KC.OS(KC.MEH),         KC.HT(KC.ENT, KC.LGUI),        KC.FD(1),                                   KC.BSPC,                        KC.SPC,           KC.OS(KC.CTRL),         XXXXXXX,       XXXXXXX,        XXXXXXX
     ],
     # Lower layer
     [
@@ -50,10 +50,10 @@ keyboard.keymap = [
     ],
     # Raise layer
     [
-     _______,      KC.DEL,       XXXXXXX,      KC.UNDS,            KC.PLUS,            KC.PGUP,                                                             XXXXXXX,      XXXXXXX,     XXXXXXX,      KC.BSLS,       KC.PIPE,      _______,
-     _______,      KC.HOME,      KC.END,       KC.MINS,            KC.EQL,             KC.PGDN,                                                             KC.APP,       KC.LEFT,     KC.UP,        KC.RIGHT,      KC.DOWN,      _______,
-     _______,      KC.LABK,      KC.RABK,      KC.LCMD(KC.C),      KC.LCMD(KC.V),      KC.SCLN,                                                             KC.MPLY,      KC.MPRV,     KC.MNXT,      KC.VOLD,       KC.VOLU,      _______,
-     XXXXXXX,      XXXXXXX,      XXXXXXX,      KC.LCTL(KC.ESC),    _______,            KC.FD(3),                                                             KC.FD(0),     _______,     _______,      XXXXXXX,       XXXXXXX,      XXXXXXX
+     KC.HT(KC.TAB, KC.CTRL(KC.ALT)),      KC.DEL,       XXXXXXX,      KC.UNDS,            KC.PLUS,            KC.PGUP,                                                             XXXXXXX,      XXXXXXX,     KC.UP,        KC.BSLS,       KC.PIPE,      _______,
+     KC.LALT,                             KC.HOME,      KC.END,       KC.MINS,            KC.EQL,             KC.PGDN,                                                             KC.APP,       KC.LEFT,     KC.DOWN,      KC.RIGHT,      KC.DOWN,      _______,
+     KC.LSFT,                             KC.LABK,      KC.RABK,      KC.LCMD(KC.C),      KC.LCMD(KC.V),      KC.SCLN,                                                             KC.MPLY,      KC.MPRV,     KC.MNXT,      KC.VOLD,       KC.VOLU,      _______,
+     XXXXXXX,                             XXXXXXX,      XXXXXXX,      KC.LCTL(KC.ESC),    _______,            KC.FD(3),                                                            KC.FD(0),     _______,     _______,      XXXXXXX,       XXXXXXX,      XXXXXXX
     ],
     # Func layer
     [
