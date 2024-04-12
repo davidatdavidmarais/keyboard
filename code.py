e68a662293b7e4ac991cc22b85ce149b32ca9565
David Marais <david@davidmarais.co.za>
2024-04-12 14:37:52 +0200

    Update code.py

--- code.py
+++ code.py
@@ -32,37 +32,54 @@ holdtap = HoldTap()
 holdtap.tap_time = 200
 keyboard.modules.append(holdtap)
 
-_______ = KC.TRNS
-XXXXXXX = KC.NO
+___ = KC.TRNS
+N_A = KC.NO
+
+# MULTI
+CA = KC.LCTRL(KC.LALT)
+CC = KC.LCMD(KC.C)
+CV = KC.LCMD(KC.V)
+# OS
+OS_ALT = KC.OS(KC.LALT)
+OS_SFT = KC.OS(KC.LSFT)
+OS_MEH = KC.OS(KC.MEH)
+# HT
+HT_ENT_GUI = KC.HT(KC.ENT, KC.LGUI)
+# LEVELS
+L0 = KC.FD(0)
+L1 = KC.FD(1)
+L2 = KC.FD(2)
+L3 = KC.FD(3)
+
 
 keyboard.keymap = [
     # Base layer
     [
-     KC.HT(KC.TAB, KC.LCTRL(KC.LALT)),                KC.Q,         KC.W,           KC.E,                  KC.R,                          KC.T,                                       KC.Y,                           KC.U,             KC.I,                   KC.O,          KC.P,           KC.ESC,
-     KC.OS(KC.LSFT),                                KC.A,         KC.S,           KC.D,                  KC.F,                          KC.G,                                       KC.H,                           KC.J,             KC.K,                   KC.L,          KC.SCLN,        KC.QUOT,
-     KC.OS(KC.LALT),                                KC.Z,         KC.X,           KC.C,                  KC.V,                          KC.B,                                       KC.N,                           KC.M,             KC.COMM,                KC.DOT,        KC.SLSH,        KC.QUES,
-     XXXXXXX,                                       XXXXXXX,      XXXXXXX,        KC.OS(KC.MEH),         KC.HT(KC.ENT, KC.LGUI),        KC.FD(1),                                   KC.BSPC,                        KC.SPC,           KC.OS(KC.LCTL),         XXXXXXX,       XXXXXXX,        XXXXXXX
+     KC.HT(KC.TAB, CA),    KC.Q,         KC.W,        KC.E,               KC.R,               KC.T,                                       KC.Y,           KC.U,         KC.I,               KC.O,          KC.P,           KC.ESC,
+     OS_SFT,               KC.A,         KC.S,        KC.D,               KC.F,               KC.G,                                       KC.H,           KC.J,         KC.K,               KC.L,          KC.SCLN,        KC.QUOT,
+     OS_ALT,               KC.Z,         KC.X,        KC.C,               KC.V,               KC.B,                                       KC.N,           KC.M,         KC.COMM,            KC.DOT,        KC.SLSH,        KC.QUES,
+     N_A,                  N_A,          N_A,         OS_MEH,             HT_ENT_GUI,         L1,                                         KC.BSPC,        KC.SPC,       KC.LCTL,            N_A,           N_A,            N_A
     ],
     # Lower layer
     [
-     KC.EXLM,             KC.AT,        KC.HASH,      KC.DLR,             KC.PERC,                     KC.CIRC,                                                                         KC.AMPR,      KC.ASTR,      KC.LPRN,      KC.RPRN,       KC.LSFT(KC.MINUS),     KC.LSFT(KC.EQL),  
-     KC.P1,               KC.P2,        KC.P3,        KC.P4,              KC.P5,                       KC.P6,                                                                           KC.P7,        KC.P8,        KC.P9,        KC.P0,         KC.MINUS,              KC.EQL,
-     KC.OS(KC.LSFT),      XXXXXXX,      KC.TILD,      KC.GRV,             KC.LBRC,                     KC.LCBR,                                                                         KC.RCBR,      KC.RBRC,      KC.LABK,      KC.RABK,       KC.QUES,               KC.QUES,
-     XXXXXXX,             XXXXXXX,      XXXXXXX,      KC.OS(KC.MEH),      KC.HT(KC.ENT, KC.LGUI),      KC.FD(2),                                                                        KC.FD(0),     KC.SPC,       KC.COLN,      XXXXXXX,       XXXXXXX,               XXXXXXX
+     KC.EXLM,             KC.AT,        KC.HASH,      KC.DLR,             KC.PERC,            KC.CIRC,                                    KC.AMPR,        KC.ASTR,      KC.LPRN,            KC.RPRN,       KC.UNDS,        KC.PPLS,  
+     KC.P1,               KC.P2,        KC.P3,        KC.P4,              KC.P5,              KC.P6,                                      KC.P7,          KC.P8,        KC.P9,              KC.P0,         KC.MINUS,       KC.EQL,
+     KC.OS(KC.LSFT),      N_A,          KC.TILD,      KC.GRV,             KC.LBRC,            KC.LCBR,                                    KC.RCBR,        KC.RBRC,      KC.LABK,            KC.RABK,       KC.QUES,        KC.QUES,
+     N_A,                 N_A,          N_A,          OS_MEH,             HT_ENT_GUI,         L2,                                         L0,             KC.SPC,       KC.COLN,            N_A,           N_A,            N_A
     ],
     # Raise layer
     [
-     KC.HT(KC.TAB, KC.LCTRL(KC.LALT)),      KC.DEL,       XXXXXXX,      KC.UNDS,            KC.PLUS,            KC.PGUP,                                                             XXXXXXX,      XXXXXXX,     KC.UP,        KC.BSLS,       KC.PIPE,      _______,
-     KC.LSFT,                             KC.HOME,      KC.END,       KC.MINS,            KC.EQL,             KC.PGDN,                                                             KC.APP,       KC.LEFT,     KC.DOWN,      KC.RIGHT,      KC.DOWN,      _______,
-     KC.LALT,                             KC.LABK,      KC.RABK,      KC.LCMD(KC.C),      KC.LCMD(KC.V),      KC.SCLN,                                                             KC.MPLY,      KC.MPRV,     KC.MNXT,      KC.VOLD,       KC.VOLU,      _______,
-     XXXXXXX,                             XXXXXXX,      XXXXXXX,      KC.LCTL(KC.ESC),    _______,            KC.FD(3),                                                            KC.FD(0),     _______,     _______,      XXXXXXX,       XXXXXXX,      XXXXXXX
+     KC.HT(KC.TAB, CA),   KC.DEL,       N_A,          KC.UNDS,            KC.PLUS,            KC.PGUP,                                    N_A,            N_A,          KC.UP,              KC.BSLS,       KC.PIPE,        ___,
+     KC.LSFT,             KC.HOME,      KC.END,       KC.MINS,            KC.EQL,             KC.PGDN,                                    KC.APP,         KC.LEFT,      KC.DOWN,            KC.RIGHT,      KC.DOWN,        ___,
+     KC.LALT,             KC.LABK,      KC.RABK,      CC,                 CV,                 KC.SCLN,                                    KC.MPLY,        KC.MPRV,      KC.MNXT,            KC.VOLD,       KC.VOLU,        ___,
+     N_A,                 N_A,          N_A,          ___,                ___,                L3,                                         L0,             ___,          ___,                N_A,           N_A,            N_A
     ],
     # Func layer
     [
-     KC.F1,         KC.F2,         KC.F3,        KC.F4,        KC.F5,        KC.F6,                                                                          KC.F7,         KC.F8,        KC.F9,         KC.F10,       KC.F11,       KC.F12,
-     KC.P0,        KC.P1,        KC.P2,        KC.P3,        KC.P4,        KC.P5,                                                                           KC.P6,        KC.P7,        KC.P8,        KC.P9,         _______,      _______,
-     _______,      XXXXXXX,       XXXXXXX,       XXXXXXX,      XXXXXXX,      XXXXXXX,                                                                        XXXXXXX,      XXXXXXX,       XXXXXXX,      XXXXXXX,       XXXXXXX,      XXXXXXX,
-     _______,      XXXXXXX,       XXXXXXX,       XXXXXXX,      XXXXXXX,      XXXXXXX,                                                                        XXXXXXX,      KC.FD(0),      XXXXXXX,      XXXXXXX,       XXXXXXX,      XXXXXXX
+     KC.F1,               KC.F2,        KC.F3,        KC.F4,              KC.F5,              KC.F6,                                      KC.F7,          KC.F8,        KC.F9,              KC.F10,        KC.F11,         KC.F12,
+     KC.P0,               KC.P1,        KC.P2,        KC.P3,              KC.P4,              KC.P5,                                      KC.P6,          KC.P7,        KC.P8,              KC.P9,         ___,            ___,
+     ___,                 N_A,          N_A,          N_A,                N_A,                N_A,                                        N_A,            N_A,          N_A,                N_A,           N_A,            N_A,
+     ___,                 N_A,          N_A,          N_A,                N_A,                N_A,                                        L0,             N_A,          N_A,                N_A,           N_A,            N_A
     ]
 ]    
 
